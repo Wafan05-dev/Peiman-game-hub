fadfffec039372bc802fefce5e53d26a4dde535c
Peiman Matin <peyman_matin.2009@yahoo.com>
2023-04-08 17:55:18 +0430

    Creat a custom hook to fetch the games.

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,32 +1,7 @@
-import { useEffect, useState } from "react";
-import { AxiosError } from "axios";
-import apiClient from "../services/api-client";
-
-interface Games {
-  id: number;
-  name: string;
-}
-
-interface FetchGamesResponse {
-  count: number;
-  results: Games[];
-}
+import useGames from "../hooks/useGames";
 
 const GameGrid = () => {
-  const [games, setGames] = useState<Games[]>([]);
-  const [errors, setErrors] = useState("");
-
-  useEffect(() => {
-    const fetchGamesGird = async () => {
-      try {
-        const { data } = await apiClient.get<FetchGamesResponse>("/games");
-        setGames([...data.results]);
-      } catch (ex) {
-        setErrors((ex as AxiosError).message);
-      }
-    };
-    fetchGamesGird();
-  }, []);
+  const { games, errors } = useGames();
 
   return (
     <>
---
+++ src/hooks/useGames.ts
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+import apiClient from "../services/api-client";
+import { AxiosError } from "axios";
+
+interface Games {
+  id: number;
+  name: string;
+}
+
+interface FetchGamesResponse {
+  count: number;
+  results: Games[];
+}
+
+const useGames = () => {
+  const [games, setGames] = useState<Games[]>([]);
+  const [errors, setErrors] = useState("");
+
+  useEffect(() => {
+    const fetchGamesGird = async () => {
+      try {
+        const { data } = await apiClient.get<FetchGamesResponse>("/games");
+        setGames([...data.results]);
+      } catch (ex) {
+        setErrors((ex as AxiosError).message);
+      }
+    };
+    fetchGamesGird();
+  }, []);
+  return { games, errors };
+};
+
+export default useGames;
