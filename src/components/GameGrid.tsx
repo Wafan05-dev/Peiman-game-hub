141bbb5619f1fb746c4cc710b3e2dcf9edb3113a
Peiman Matin <peyman_matin.2009@yahoo.com>
2023-04-14 12:18:11 +0430

    Refactor game grid.

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,4 +1,4 @@
-import { SimpleGrid } from "@chakra-ui/react";
+import { SimpleGrid, Text } from "@chakra-ui/react";
 import { Games } from "../hooks/useGames";
 import GameCards from "./GameCards";
 import GameCardSkeleton from "./GameCardSkeleton";
@@ -12,21 +12,20 @@ interface Props {
 const GameGrid = ({ games, errors, isLoading }: Props) => {
   const skeletons = [1, 2, 3, 4, 5, 6];
 
+  if (errors) return <Text>{errors}</Text>;
+
   return (
-    <>
-      {errors && <p>{errors}</p>}
-      <SimpleGrid
-        columns={{ sm: 1, md: 2, lg: 3, xl: 3 }}
-        spacing={3}
-        padding="10px"
-      >
-        {isLoading &&
-          skeletons.map((skeleton) => <GameCardSkeleton key={skeleton} />)}
-        {games.map((game) => (
-          <GameCards key={game.id} game={game}></GameCards>
-        ))}
-      </SimpleGrid>
-    </>
+    <SimpleGrid
+      columns={{ sm: 1, md: 2, lg: 3, xl: 3 }}
+      spacing={3}
+      padding="10px"
+    >
+      {isLoading &&
+        skeletons.map((skeleton) => <GameCardSkeleton key={skeleton} />)}
+      {games.map((game) => (
+        <GameCards key={game.id} game={game}></GameCards>
+      ))}
+    </SimpleGrid>
   );
 };
 
